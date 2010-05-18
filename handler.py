2b059a9959578d254f72a6441fdcdf472b641b79
Matt Pizzimenti <mjpizz@gmail.com>
2010-05-18 16:28:02 -0700

    version bump, forgot that

--- handler.py
+++ handler.py
@@ -6,7 +6,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-VERSION = '12'
+VERSION = '13'
 
 
 class NewsletterEntry(db.Model):
