98cca7d5d8d13845591543a4bf23ad90b67cd7c1
Matt Pizzimenti <mjpizz@gmail.com>
2010-05-18 19:25:16 -0700

    forgot the version bump again, maybe its time to reconsider this caching method

--- handler.py
+++ handler.py
@@ -6,7 +6,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-VERSION = '16'
+VERSION = '17'
 
 
 class NewsletterEntry(db.Model):
