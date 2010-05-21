6a9020832665f1561e233df0b5f23d7c4b3bd825
Matt Pizzimenti <mjpizz@gmail.com>
2010-05-21 16:54:02 -0700

    bumped cache version

--- handler.py
+++ handler.py
@@ -6,7 +6,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-VERSION = '39'
+VERSION = '40'
 
 
 class NewsletterEntry(db.Model):
